51c9f75118c4bd4a4d5548e83a26530558b65e53
YaoHuiJi <yaohj@hotmail.com>
2023-11-07 21:51:52 +0800

    init

---
+++ main.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func quit_to_main():
+	print("Quit to main screen")
+
+
+func _on_main_menu_open_game(gameId,gameName):
+	$MainMenu.hide()
+	
+	match gameId:
+		0:
+			print("打开游戏: ", gameName)
+			var tictactoe = preload("res://games/tictactoe/TicTacToe.tscn").instantiate()
+			add_child(tictactoe)
+		_:
+			print("游戏还没实现：",gameId)
+		
+	pass # Replace with function body.
